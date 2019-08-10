ba1c6749bd767ed4c3dacf28646f56e06cf6233d
AAsriyan <aasriyan@protonmail.com>
2019-08-09 23:40:48 -0400

    different types of graphs

---
+++ DS/GRAPHS/typesOfGraphs.js
@@ -0,0 +1,8 @@
+// Edge list
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+// Adjacent list
+const graphB = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent Matrix
+const graphC = [[0, 0, 1, 0], [0, 0, 1, 1], [1, 1, 0, 1], [0, 1, 1, 0]];
