3a2d548727328c7f137431d712b8021386788215
Arshak Asriyan <aasriyan01@gmail.com>
2019-08-20 22:53:25 -0400

    warmup - jumping clouds

---
+++ HACKERRANK/Warmup/jumpClouds.js
@@ -0,0 +1,14 @@
+function jumpingOnClouds(c) {
+    let jumps = 0;
+  
+    for (let i = 0; i < c.length - 1; i++) {
+      if (c[i + 2] === 0) {
+        jumps++
+        i += 1
+      } else {
+        jumps++
+      }
+    }
+  
+    return jumps
+  }
